d628a6e87b26318224151373cfbb3cb22603f4f9
rei0515reten <glay5152@gmail.com>
2023-05-06 15:13:23 +0900

    update Dockerfile env.sh

--- image/env.sh
+++ image/env.sh
@@ -2,4 +2,4 @@
 
 export PATH=$PATH:/opt/riscv/bin
 export PATH=$PATH:/opt/riscv/spike/bin 
-alias pk="ln -s /opt/riscv/pk/riscv64-unknown-elf/bin/pk ./pk"
+alias pk="ln -s /opt/riscv/pk/riscv64-unknown-linux-gnu/bin/pk ./pk"
